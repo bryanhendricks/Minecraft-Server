899cab1aa7c92913b4c2429b7544e5da06186fb4
bryanhendricks <bryanhendricks7@gmail.com>
2022-01-08 21:05:13 -0800

    Added more details to status page

--- web_server_2/web_server.py
+++ web_server_2/web_server.py
@@ -3,10 +3,14 @@ from flask import Flask, request, redirect
 import time
 sys.path.append(os.path.dirname(__file__))
 from src import connect_aws, get_server_status, start_server
+from mcstatus import MinecraftServer
 
 
 MINECRAFT_SERVER_INSTANCE_ID = 'i-00c2d1af54569b884'
-PUBLIC_IP_ADDRESS = '54.241.254.8'
+WEBSERVER_PUBLIC_IP_ADDRESS = '54.241.254.8'
+MINECRAFT_SERVER_IP = '13.52.129.204'
+MINECRAFT_SERVER_PORT = 25565
+
 
 app = Flask(__name__)
 
@@ -32,7 +36,7 @@ def home_page():
                 # An error occurred starting the server - display an error message
                 return '<h1>Something went wrong while starting the server, tell Bryan to fix it</h1>'
             # Redirect to server status page
-            return redirect('http://%s/status' % PUBLIC_IP_ADDRESS)
+            return redirect('http://%s/status' % WEBSERVER_PUBLIC_IP_ADDRESS)
 
 
 @app.route('/status')
@@ -40,6 +44,16 @@ def status_page():
     print('Getting server status')
     # Get and display the server status
     server_status = get_server_status(connect_aws(), MINECRAFT_SERVER_INSTANCE_ID)
+    if server_status == 'running':
+        try:
+            server = MinecraftServer(MINECRAFT_SERVER_IP, MINECRAFT_SERVER_PORT)
+            mc_status = server.status()
+            player_count = mc_status.players.online
+            # Server is up
+            server_status = 'running<br>Player count: ' + str(player_count)
+        except:
+            # Server is down
+            server_status = 'starting'
     return '<h1>Server status: %s</h1>' % server_status
 
 
