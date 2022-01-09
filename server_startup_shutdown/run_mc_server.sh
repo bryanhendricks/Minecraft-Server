954feb2016217cbe22a4718cf39654ef0bbd3a96
bryanhendricks <bryanhendricks7@gmail.com>
2022-01-08 20:55:03 -0800

    Bugfix, update deployment instructions

--- server_startup_shutdown/run_mc_server.sh
+++ server_startup_shutdown/run_mc_server.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-JAR_PATH='/home/ubuntu/minecraft-server/paper-1.18.1-140.jar'
-tmux new-session -d -s MinecraftServer 'cd /home/ubuntu/minecraft-server/minecraft; java -Xms2G -Xmx14G -jar $JAR_PATH nogui; exec bash'
+tmux new-session -d -s MinecraftServer 'cd /home/ubuntu/minecraft-server/minecraft; java -Xms2G -Xmx14G -jar /home/ubuntu/minecraft-server/paper-1.18.1-140.jar nogui; exec bash'
